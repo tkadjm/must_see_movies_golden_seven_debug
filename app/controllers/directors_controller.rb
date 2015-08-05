1257996fd8ef98c12c6176b6f31011a7653f3873
tkadjm <tkeane@chicagobooth.edu>
2015-08-05 17:09:43 -0500

    wip 05

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -22,25 +22,25 @@ class DirectorsController < ApplicationController
     redirect_to("http://localhost:3000/directors")
   end
 
+  def destroy
+    @director = Director.find(params[:id])
+    @director.destroy
+  end
+
   def edit_form
     @director = Director.find(params[:id])
   end
 
   def update_row
-    @director = Director.find(params[:id])
 
-    @director.dob = params[:dob]
-    @director.name = params[:name]
-    @director.bio = params[:bio]
-    @director.image_url = params[:image_url]
+    d = Director.find(params["id"].to_i)
+    d.name = params["the_name"]
+    d.dob = params["the_dob"]
+    d.bio = params["the_bio"]
+    d.image_url = params["the_image_url"]
+    d.save
+    redirect_to("http://localhost:3000/directors")
 
-    @director.save
-
-   redirect_to("http://localhost:3000/directors")
   end
 
-  def destroy
-    @director = Director.find(params[:id])
-    @director.destroy
-  end
 end
